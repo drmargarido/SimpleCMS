a0cb4bb462fb7360408c2166b567a81b5a5c9d87
Daniel Margarido <drmargarido@gmail.com>
2018-01-16 00:23:27 +0000

    Added url mapping to the index page. Added the necessary blocks and removed unecessary content from the base_page

--- simple_cms/manager/urls.py
+++ simple_cms/manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from manager.views import article_page, article_page_by_link
+from manager.views import index_page, article_page, article_page_by_link
 
 urlpatterns = [
+	path("", index_page),
 	path("<int:article_id>/", article_page),
 	path("<str:link>/", article_page_by_link),
 ]
